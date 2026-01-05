383fe38f9d1c69e6bff3224fd3f04e25518d0658
Elvis <elvisthomas2724@gmail.com>
2026-01-05 10:18:45 +0530

    Add project files

--- tempcode.c
+++ tempcode.c
@@ -1,3 +1,13 @@
-while ((ch = fgetc(f1)) != EOF) {
+#include <stdio.h>
+
+int main() {
+    FILE *f1 = fopen("filename.txt", "r");
+    int ch;
+    
+    while ((ch = fgetc(f1)) != EOF) {
         printf("%c", ch);  // Temporary: just print characters
     }
+    
+    fclose(f1);
+    return 0;
+}
